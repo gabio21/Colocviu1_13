6b347ab68935d118a5e07ab8c754a17f68a3f6e7
Gabriel <gabrieloprinoiu@gmail.com>
2020-04-13 11:05:34 +0300

    Rezolvare A

---
+++ task/app/src/main/java/ro/pub/cs/systems/eim/colocviu1_13/colocviu1_13MainActivity.java
@@ -0,0 +1,13 @@
+package ro.pub.cs.systems.eim.colocviu1_13;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class colocviu1_13MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colocviu1_13_main);
+    }
+}
